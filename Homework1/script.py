3950159ec13a794fe3d2dd06217895c8fc0052d6
Arun Kumar <rarunkumar15112k@gmail.com>
2023-01-15 16:39:02 -0500

    Update script.py

--- Homework1/script.py
+++ Homework1/script.py
@@ -1,4 +1,34 @@
-def main():
-    print("Not yet implemented")
+import math
+import sys 
 
-main()
+class SYM():
+    def __init__(self):
+        self.n = 0
+        self.has = {}
+        self.most = 0
+        self.mode = None
+    
+    def add(self,x):
+        if x != "?":
+            self.n += 1
+            self.has[x]  = 1 + self.has.get(x,0)
+            if self.has[x] > self.most:
+                self.most , self.mode = self.has[x], x
+    
+    def mid(self):
+        return self.mode 
+    
+    def div(self):
+        def fun(p):
+            return p*math.log(p,2)
+        e = 0
+        for _,n in self.has.items():
+            e = e + fun(n/self.n)
+        return -e
+
+# Testing    
+sym = SYM()
+for x in ["a","a","a","a","b","b","c"]:
+    sym.add(x)
+print(sym.mid()) # a 
+print(sym.div()) # 1.37
