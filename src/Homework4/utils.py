a90f6d0ae676249bf65bde958b5097f8847ee47e
amsauerb <amsauer13@gmail.com>
2023-02-09 16:41:38 -0500

    Finished most of the util functions

--- src/Homework4/utils.py
+++ src/Homework4/utils.py
@@ -1,5 +1,8 @@
 import math
 import re
+import copy
+
+import data
 
 
 seed = 937162211
@@ -8,10 +11,8 @@ seed = 937162211
 def coerce(s):
     """
     Coerces a str s into an int, float, bool, or trimmed str.
-
     Args:
         s (str): Str to be coerced into an int, float, bool, or trimmed str.
-
     Returns:
         int/float/bool/str: int, float, bool, or trimmed str version of s.
     """
@@ -33,7 +34,6 @@ def coerce(s):
 def csv(sFilename, fun):
     """
     Calls the function fun on the rows after coercing the cell text.
-
     Args:
         sFilename (str): Filename of the csv file.
         fun (function): Function to be performed on each row of the csv file.
@@ -54,12 +54,10 @@ def csv(sFilename, fun):
 def cosine(a, b, c):
     """
     Finds x and y from the line connecting a to b.
-
     Args:
         a (float): a value to calculate the cosine from.
         b (float): b value to calculate the cosine from.
         c (float): c value to calculate the cosine from.
-
     Returns:
         int/float: x from the line connecting a to b.
         float: y from the line connecting a to b.
@@ -79,11 +77,9 @@ def cosine(a, b, c):
 def rand(lo=0, hi=1):
     """
     Generates a random float between lo (inclusive) and hi (not inclusive).
-
     Args:
         lo (int, optional): Lower bound for the random float generation. Defaults to 0.
         hi (int, optional): Upper bound for the random float generation. Defaults to 1.
-
     Returns:
         float: Random float between lo (inclusive) and hi (not inclusive).
     """
@@ -97,11 +93,9 @@ def rand(lo=0, hi=1):
 def rint(lo=0, hi=1):
     """
     Generates a random int between lo (inclusive) and hi (not inclusive).
-
     Args:
         lo (int, optional): Lower bound for the random int generation. Defaults to 0.
         hi (int, optional): Upper bound for the random int generation. Defaults to 1.
-
     Returns:
         int: Random int between lo (inclusive) and hi (not inclusive).
     """
@@ -112,11 +106,9 @@ def rint(lo=0, hi=1):
 def many(t, n):
     """
     Returns n ROWs from t.
-
     Args:
         t (list): List to return the ROWs from.
         n (int): Number of ROWs to be returned.
-
     Returns:
         list: List of n ROWs from t.
     """
@@ -127,10 +119,8 @@ def many(t, n):
 def any(t):
     """
     Returns a random ROW from t.
-
     Args:
         t (list): List to return the random ROW from.
-
     Returns:
         ROW: Random row from t.
     """
@@ -142,10 +132,8 @@ def any(t):
 def copy(t):
     """
     Deep copies t.
-
     Args:
         t (_type_): _description_
-
     Returns:
         _type_: _description_
     """
@@ -157,87 +145,121 @@ def copy(t):
 def transpose(t):
     """
     _summary_
-
     Args:
         t (_type_): _description_
-
     Returns:
         _type_: _description_
     """
 
-    print("Not yet implemented")
-    return 0
+    u = {}
+    for i in len(t[1]):
+        u[i] = {}
+        for j in len(t):
+            u[i][j] = t[j][i]
+    return u
 
 
 def repCols(cols):
     """
     _summary_
-
     Args:
         cols (_type_): _description_
-
     Returns:
         _type_: _description_
     """
 
-    print("Not yet implemented")
-    return 0
+    cols = copy.deepcopy(cols)
+    for _, col in enumerate(cols):
+        col[len(col)] = col[1] + ":" + col[len(col)]
+        j = 2
+        for j in len(col):
+            col[j - 1] = col[j]
+        col[len(col)] = None
+    def fun(k,v):
+        return "Num" + k
+    cols.insert(1, map(fun, cols[1]))
+    cols[1][len(cols[1])] = "thingX"
+    return data.DATA(cols)
 
 
 def repRows(t, rows, u):
     """
     _summary_
-
     Args:
         t (_type_): _description_
         rows (_type_): _description_
         u (_type_): _description_
-
     Returns:
         _type_: _description_
     """
 
-    print("Not yet implemented")
-    return 0
+    rows = copy.deepcopy(rows)
+    for j,s in enumerate(rows[len(rows)]):
+        rows[1][j] = rows[1][j] + ":" + s
+    rows[len(rows)] = None
+    row = []
+    for n,row in enumerate(rows):
+        if n == 1:
+            row.append("thingX")
+        else:
+            u = t.rows[len(t.rows) - n + 2]
+            row.append(u[len(u)])
+    return data.DATA(rows)
 
 
-def repPlace(data, n=20, g={}, maxy=0):
+def repPlace(data, n=20, g={}):
     """
     _summary_
-
     Args:
         data (_type_): _description_
         n (int, optional): _description_. Defaults to 20.
         g (dict, optional): _description_. Defaults to {}.
         maxy (int, optional): _description_. Defaults to 0.
-
     Returns:
         _type_: _description_
     """
 
-    print("Not yet implemented")
-    return 0
+    i = 1
+    for i in n+1:
+        g[i] = {}
+        j = 1
+        for j in n+1:
+            g[i][j] = " "
+    maxy = 0
+    print("")
+    for r, row in enumerate(data.rows):
+        c = str(64+r)
+        print(c, row.cells[len(row.cells)])
+        x,y = row.x*n//1, row.y*n//1
+        maxy = math.max(maxy, y + 1)
+        g[y+1][x+1] = c
+    print("")
+    y = 1
+    for y in maxy:
+        str(g[y])
 
 
-def repgrid(file):
+def repgrid(sFile):
     """
     _summary_
-
     Args:
-        file (_type_): _description_
-
+        sFile (_type_): _description_
     Returns:
         _type_: _description_
     """
 
-    print("Not yet implemented")
+    t = doFile(sFile)
+    rows = repRows(t, transpose(t.cols))
+    cols = repCols(t.cols)
+    show(rows.cluster())
+    show(cols.cluster())
+    repPlace(rows)
     return 0
 
 
 def show(node, what, cols, nPlaces, lvl=0):
     """
     Prints the tree version of DATA.
-
     Args:
         node (dict): Dictionary of DATA to be printed.
         what (str): Either "mid" or "div".
@@ -247,4 +269,7 @@ def show(node, what, cols, nPlaces, lvl=0):
     """
 
     print("Not yet implemented")
+    return 0
+
+def doFile(sFile):
     return 0
